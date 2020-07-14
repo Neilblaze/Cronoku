d8e63340ed593c1fb6775fa4999d5321381aced7
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-07-14 16:21:20 +0530

    urix fetched res.end();

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const http = require("http");
+const https = require("https");
+const CronJob = require("cron").CronJob;
+
+const server = http.createServer((req, res) => {
+  res.end();
+}).listen(process.env.PORT);
+
+const job = new CronJob("0 */15 1-14 * * *", function() {
+  https.get(process.env.LOCAL); // keep myself alive
+  https.get(process.env.SERVER); // keep another server alive
+  console.log(`ping ${process.env.LOCAL}`);
+  console.log(`ping ${process.env.SERVER}`);
+});
+
+job.start();
+console.log("Running!");
